2a01e3185bee0daac856c4994bdc139961894385
gaomengmeng <gmm_liubeibei0325@163.com>
2022-06-27 15:45:50 +0800

    管理系统 init

--- react-egg-server/my-demo/src/view/Profile/index.js
+++
@@ -1,8 +0,0 @@
-import React, { useEffect } from "react";
-import requests from "../../utils/request";
-export default function Home() {
-  useEffect(() => {
-    requests.get("/home");
-  });
-  return <div>首页1</div>;
-}
